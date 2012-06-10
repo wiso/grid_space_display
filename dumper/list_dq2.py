da8300b62c21c0d76301d160607a7a74dcd440e0
Ruggero Turra <ruggero.turra@cern.ch>
2012-06-10 12:18:43 +0000

    output basename

--- dumper/list_dq2.py
+++ dumper/list_dq2.py
@@ -294,5 +294,6 @@ if __name__ == "__main__":
     with open(output_complete_filename, "w") as f:
         f.write(xml)
 
-    print output_complete_filename # this is the only output to stdout
+    # this is the only output to stdout
+    print os.path.basename(output_complete_filename)
     logger.info("exiting")
