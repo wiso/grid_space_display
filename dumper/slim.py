31c6f3b092a8b7856ac077182399f182e9dfc78a
Ruggero Turra <ruggero.turra@cern.ch>
2014-06-13 08:23:09 +0000

    fix slim

--- dumper/slim.py
+++ dumper/slim.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from stat import S_ISREG, ST_CTIME, ST_MODE
 import os
 import subprocess
@@ -25,12 +27,10 @@ def slim(filename):
 if __name__ == "__main__":
     from optparse import OptionParser
     parser = OptionParser()
-    parser.add_option("-d", "--directory",
-                      help="input directory with the xmls")
     parser.add_option("-n", "--number", type=int, default=1,
                       help="number of newest xml to don't slim")
     (options, args) = parser.parse_args()
-    files_to_slim = list_xmlfiles(options.directory)[:-options.number]
+    files_to_slim = list_xmlfiles(args[0])[:-options.number]
     files_to_slim = (f for f in files_to_slim if not isslimmed(f))
     for f in files_to_slim:
         print "slimming", f
