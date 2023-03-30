bc4cc2631bd77ec72eb33571e90d5d0d13b419a1
Ruggero Turra <ruggero.turra@cern.ch>
2023-03-30 17:34:09 +0200

    clear code

--- interface/create_json.py
+++ interface/create_json.py
@@ -1,11 +1,15 @@
-import pandas as pd
-import numpy as np
+import json
+
 import cufflinks as cf
+import numpy as np
+import pandas as pd
+from pandas.io.pytables import HDFStore
+
 cf.go_offline()
 
-import json
-class NumpyEncoder(json.JSONEncoder):
 
+
+class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
         """If input object is an ndarray it will be converted into a dict
         holding dtype, shape and the data, base64 encoded.
@@ -15,39 +19,51 @@ class NumpyEncoder(json.JSONEncoder):
         # Let the base class default method raise the TypeError
         return json.JSONEncoder(self, obj)
 
-from pandas.io.pytables import HDFStore
-store = HDFStore('store.h5')
+
+
+store = HDFStore("store.h5")
 
 data = []
-for k in store.keys():
+for k in list(store.keys()):
     try:
         d = store.get(k)
-        d['timestamp'] = pd.to_datetime(k.split("_")[1], format='%d%m%Y')
+        d["timestamp"] = pd.to_datetime(k.split("_")[1], format="%d%m%Y")
         data.append(d)
     except Exception as e:
-        print "Problem reading", k
-        print e
+        print("Problem reading", k)
+        print(e)
 store.close()
 
 data = pd.concat(data)
-data = data.set_index(['timestamp', 'owner'])
+data = data.set_index(["timestamp", "owner"])
 
 # stack plot
-data_to_plot = data['size'].unstack().fillna(0)
-dataplot = data_to_plot.iplot(kind='scatter', fill=True, asFigure=True)
-data.iplot(data=dataplot['data'])
+data_to_plot = data["size"].unstack().fillna(0)
+dataplot = data_to_plot.iplot(kind="scatter", fill=True, asFigure=True)
+data.iplot(data=dataplot["data"])
 
-f_json_data = open('data_scatter.json', 'w')
-json_data = json.dump(dataplot['data'], f_json_data, cls=NumpyEncoder)
-f_json_data.close()
+with open("data.json", "w") as f_json_data:
+    plt_json = dataplot.to_json(validate=True, pretty=True)
+    f_json_data.write(plt_json)
 
 # pie plot
-latest_data = data[data.index.get_level_values('timestamp') == data.index.get_level_values('timestamp').max()]
-dataplot = latest_data.reset_index().iplot(kind='pie', labels='owner', values='size', hole='0.4', sort=True, textinfo='percent', asFigure=True)
-dataplot['data'][0]['text'] = ["%.2f Gb" % xx for xx in dataplot['data'][0]['values']]
-dataplot['data'][0]['hoverinfo'] = 'text+label'
-data.iplot(data=dataplot['data'])
+latest_data = data[
+    data.index.get_level_values("timestamp") == data.index.get_level_values("timestamp").max()
+]
+dataplot = latest_data.reset_index().iplot(
+    kind="pie",
+    labels="owner",
+    values="size",
+    hole=0.4,
+    sort=True,
+    textinfo="percent",
+    asFigure=True,
+)
+dataplot["data"][0]["text"] = ["%.2f Gb" % xx for xx in dataplot["data"][0]["values"]]
+dataplot["data"][0]["hoverinfo"] = "text+label"
+data.iplot(data=dataplot["data"])
 
-f_json_data = open('data_pie.json', 'w')
-json_data = json.dump(dataplot['data'], f_json_data, cls=NumpyEncoder)
-f_json_data.close()
+
+with open("data_pie.json", "w") as f_json_data:
+    plt_json = dataplot.to_json(pretty=True)
+    f_json_data.write(plt_json)
--- interface/dump.py
+++ interface/dump.py
@@ -1,93 +1,126 @@
 # to compress: ptrepack --chunkshape=auto --propindexes --complevel=9 --complib=blosc store.h5 c.h5
 
+import datetime
+import logging
+import multiprocessing.dummy
+import ssl
+import sys
+import urllib.error
+import urllib.parse
+import urllib.request
+from multiprocessing.dummy import Lock
+
 import numpy as np
 import pandas as pd
+
 # WARNING for pandas >=0.17: https://github.com/pydata/pandas/issues/11786
-import logging
-import datetime
-import urllib2
-import sys
-import json
-import cufflinks as cf
-cf.go_offline()
 
 logging.basicConfig(level=logging.INFO)
 
 # TODO: solve this
-import ssl
-if hasattr(ssl, '_create_default_https_context'):
+
+if hasattr(ssl, "_create_default_https_context"):
     ssl._create_default_https_context = ssl._create_unverified_context
 
-Gb = 1024. ** 3
-Tb = 1024. ** 4
+Gb = 1024.0**3
+Tb = 1024.0**4
 
-
-class NumpyEncoder(json.JSONEncoder):
-    def default(self, obj):
-        """If input object is an ndarray it will be converted into a dict
-        holding dtype, shape and the data, base64 encoded.
-        """
-        if isinstance(obj, np.ndarray):
-            return obj.tolist()
-        # Let the base class default method raise the TypeError
-        return json.JSONEncoder(self, obj)
 
 def group_by_owner(data):
-    default_actions = {'owner':'count', 'life_days': 'mean', 'age_days': 'mean', 'last_accessed_days': 'mean', 'size': lambda x: np.sum(x) / Tb}
+    default_actions = {
+        "owner": "count",
+        "life_days": "mean",
+        "age_days": "mean",
+        "last_accessed_days": "mean",
+        "size": lambda x: np.sum(x) / Tb,
+    }
     actions = {k: default_actions[k] for k in data.columns if k in default_actions}
     result = data.groupby("owner").agg(actions)
-    result.columns = ['ndatasets'] + list(result.columns)[1:]
+    result.columns = ["ndatasets"] + list(result.columns)[1:]
     return result
 
 
 def get_data(rse, date, **kwargs):
-    datestr = date.strftime('%d-%m-%Y')
+    datestr = date.strftime("%d-%m-%Y")
     url = "https://rucio-hadoop.cern.ch/consistency_datasets?rse=%s&date=%s" % (rse, datestr)
-    return to_pandas(url, dateformat='ms' if date >= datetime.datetime(2015, 7, 31) else 'string', **kwargs)
+    return to_pandas(
+        url, dateformat="ms" if date >= datetime.datetime(2015, 7, 31) else "string", **kwargs
+    )
 
 
-def to_pandas(filename, dateformat='ms', noderived=False):
+def to_pandas(filename, dateformat="ms", noderived=False):
     def conv(s):
-        s = set(s.split(','))
-        to_remove = 'panda', 'root'
+        s = set(s.split(","))
+        to_remove = "panda", "root"
         for name in to_remove:
             if name in s:
                 s.remove(name)
-        s = ','.join(s)
+        s = ",".join(s)
         return s
+
     try:
         if noderived:
-            data = pd.read_csv(filename, sep='\t', header=None,
-                               converters={3: conv},
-                               usecols=[0,1,2,3,4]
-                              )
+            data = pd.read_csv(
+                filename, sep="\t", header=None, converters={3: conv}, usecols=[0, 1, 2, 3, 4]
+            )
             data.columns = ["RSE", "scope", "name", "owner", "size"]
         else:
-            if dateformat == 'ms':
-                names = ("RSE", "scope", "name", "owner", "size", "creation_date", "last_accessed_date")
-                data = pd.read_csv(filename, sep='\t', header=None,
-                                   parse_dates=["creation_date", "last_accessed_date"],
-                                   date_parser=lambda _:pd.to_datetime(float(_), unit='ms'),
-                                   converters={"owner": conv},
-                                   names=names)
+            if dateformat == "ms":
+                names = (
+                    "RSE",
+                    "scope",
+                    "name",
+                    "owner",
+                    "size",
+                    "creation_date",
+                    "last_accessed_date",
+                )
+                data = pd.read_csv(
+                    filename,
+                    sep="\t",
+                    header=None,
+                    parse_dates=["creation_date", "last_accessed_date"],
+                    date_parser=lambda _: pd.to_datetime(float(_), unit="ms"),
+                    converters={"owner": conv},
+                    names=names,
+                )
 
-            elif dateformat == 'string':
-                names = ("RSE", "scope", "name", "owner", "size", "creation_date", "last_accessed_date")
+            elif dateformat == "string":
+                names = (
+                    "RSE",
+                    "scope",
+                    "name",
+                    "owner",
+                    "size",
+                    "creation_date",
+                    "last_accessed_date",
+                )
 
-                data = pd.read_csv(filename, sep='\t', header=None,
-                                   parse_dates=['creation_date', 'last_accessed_date'],
-                                   converters={"owner": conv},
-                                   names=names)
+                data = pd.read_csv(
+                    filename,
+                    sep="\t",
+                    header=None,
+                    parse_dates=["creation_date", "last_accessed_date"],
+                    converters={"owner": conv},
+                    names=names,
+                )
     except Exception as ex:
-        if type(ex) != urllib2.HTTPError:
-            print "cannot parse file from %s: %s" % (filename, str(ex.msg))
+        if not isinstance(ex, urllib.error.HTTPError):
+            print(
+                ("cannot parse file from %s: %s" % (filename, str(ex.msg)))
+            )  # pylint: disable=E1101
+
         raise
 
     if not noderived:
         now = datetime.datetime.now()
-        data.loc[data['creation_date'].notnull(), 'age_days'] = (now - data['creation_date']).dt.days
-        data.loc[data['last_accessed_date'].notnull(), 'last_accessed_days'] = (now - data['last_accessed_date']).dt.days
-        data['life_days'] = data['age_days'] - data['last_accessed_days']
+        data.loc[data["creation_date"].notnull(), "age_days"] = (
+            now - data["creation_date"]
+        ).dt.days
+        data.loc[data["last_accessed_date"].notnull(), "last_accessed_days"] = (
+            now - data["last_accessed_date"]
+        ).dt.days
+        data["life_days"] = data["age_days"] - data["last_accessed_days"]
 
     return data
 
@@ -102,29 +135,30 @@ def fetch_safe(date, rse):
 def valid_date(s):
     try:
         return datetime.datetime.strptime(s, "%Y-%m-%d")
-    except ValueError:
+    except ValueError as exc:
         msg = "Not a valid date: '{0}'.".format(s)
-        raise argparse.ArgumentTypeError(msg)
+        raise argparse.ArgumentError(msg) from exc
 
-from multiprocessing.dummy import Lock
+
 write_lock = Lock()
 
 if __name__ == "__main__":
     import argparse
-    parser = argparse.ArgumentParser(description='Dump storage usage.')
-    parser.add_argument('--rse')
-    parser.add_argument('--yesterday', action='store_true')
-    parser.add_argument('--ndays', type=int, default=1, help='numer of days to dump, default=1')
-    parser.add_argument('--start', type=valid_date, help='start date, format= YYYY-MM-DD')
-    parser.add_argument('--end', type=valid_date, help='end date, format= YYYY-MM-DD')
-    parser.add_argument('--nquery', type=int, help='number of concurrent query', default=50)
-    parser.add_argument('--overwrite', action='store_true')
+
+    parser = argparse.ArgumentParser(description="Dump storage usage.")
+    parser.add_argument("--rse")
+    parser.add_argument("--yesterday", action="store_true")
+    parser.add_argument("--ndays", type=int, default=1, help="numer of days to dump, default=1")
+    parser.add_argument("--start", type=valid_date, help="start date, format= YYYY-MM-DD")
+    parser.add_argument("--end", type=valid_date, help="end date, format= YYYY-MM-DD")
+    parser.add_argument("--nquery", type=int, help="number of concurrent query", default=50)
+    parser.add_argument("--overwrite", action="store_true")
     args = parser.parse_args()
 
     if args.rse is None:
-        milano_rse = "INFN-MILANO-ATLASC_LOCALGROUPDISK"
-        print "WARNING: rse not specified, use %s" % milano_rse
-        args.rse = milano_rse
+        MILANO_RSE = "INFN-MILANO-ATLASC_LOCALGROUPDISK"
+        print("WARNING: rse not specified, use %s" % MILANO_RSE)
+        args.rse = MILANO_RSE
 
     if args.yesterday:
         yesterday = datetime.date.today() - datetime.timedelta(days=1)
@@ -135,15 +169,13 @@ if __name__ == "__main__":
         datelist = pd.date_range(start=args.start, periods=args.ndays).tolist()
     elif args.end is not None and args.ndays is not None:
         datelist = pd.date_range(end=args.end, periods=args.ndays).tolist()
+    else:
+        raise ValueError("please specify a date range")
 
     if len(datelist) == 0:
         raise ValueError("date range not valid")
 
     logging.info("dumping from %s to %s", datelist[0], datelist[-1])
-
-    import multiprocessing.dummy
-
-    p = multiprocessing.dummy.Pool(args.nquery)
 
     class Monitor(object):
         def __init__(self, ntotal):
@@ -177,19 +209,21 @@ if __name__ == "__main__":
                 self.display()
 
         def display(self):
-            msg = "\r{} ({})/{} errors: {}".format(self.ndone, self.nrunning, self.ntotal, self.nerror)
+            msg = "\r{} ({})/{} errors: {}".format(
+                self.ndone, self.nrunning, self.ntotal, self.nerror
+            )
             with self.writing_lock:
-                sys.stdout.write('\r' + ' '*len(msg))
+                sys.stdout.write("\r" + " " * len(msg))
                 sys.stdout.flush()
                 sys.stdout.write(msg)
                 sys.stdout.flush()
 
         def close(self):
-            print
-            print "list of errors"
+            print()
+            print("list of errors")
             for msg in self.msg_errors:
-                print msg
-            print
+                print(msg)
+            print()
 
     from functools import partial
 
@@ -202,15 +236,15 @@ if __name__ == "__main__":
                 return result
             except Exception as ex:
                 msg = str(ex)
-                if isinstance(ex, urllib2.HTTPError):
+                if isinstance(ex, urllib.error.HTTPError):
                     msg = "code %s for url %s" % (ex.code, ex.url)
                 monitor.error(msg)
-        return w
 
+        return w
 
     def wrap_write(f, storage, overwrite=False):
         def w(date, *args, **kwargs):
-            key = date.strftime('userdata_%d%m%Y')
+            key = date.strftime("userdata_%d%m%Y")
             if not overwrite and key in storage:
                 return
             value = f(date, *args, **kwargs)
@@ -218,41 +252,18 @@ if __name__ == "__main__":
                 with write_lock:
                     storage[key] = value
                     storage.flush(fsync=True)
-                    print value
+
         return w
 
     monitor = Monitor(len(datelist))
     from pandas import HDFStore
-    store = HDFStore('store.h5', complevel=9)
-    fmap = wrap_monitor(wrap_write(partial(fetch_safe, rse=args.rse), store, overwrite=args.overwrite),
-                        monitor)
-    p.map(fmap, datelist)
+
+    store = HDFStore("store.h5", complevel=9)
+    fmap = wrap_monitor(
+        wrap_write(partial(fetch_safe, rse=args.rse), store, overwrite=args.overwrite), monitor
+    )
+    with multiprocessing.dummy.Pool(args.nquery) as pool:
+        pool.map(fmap, datelist)
     monitor.close()
     logging.info("closing file")
     store.close()
-
-    logging.info("trying to open output")
-    store = HDFStore('store.h5')
-    data = []
-
-    for k in store.keys():
-        try:
-            d = store.get(k)
-            d['timestamp'] = pd.to_datetime(k.split("_")[1], format='%d%m%Y')
-            data.append(d)
-        except Exception as e:
-            print "Problem reading", k
-            print e
-    store.close()
-    data = pd.concat(data)
-    data = data.set_index(['timestamp', 'owner'])
-
-    data_to_plot = data['size'].unstack().fillna(0)
-    dataplot = data_to_plot.iplot(kind='area', fill=True, asFigure=True)
-    for d in dataplot['data']:
-        d['hoverinfo'] = 'text+x+name'
-        d['text'] = ["%.2f Gb" % xx for xx in data_to_plot[d['name']].tolist()]
-    data.iplot(data=dataplot['data'])
-    f_json_data = open('data.json', 'w')
-    json_data = json.dump(dataplot['data'], f_json_data, cls=NumpyEncoder)
-    f_json_data.close()
