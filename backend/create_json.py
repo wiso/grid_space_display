5801feb3db5f99b38cfd7986c4449c5482a81347
Ruggero Turra <ruggero.turra@cern.ch>
2023-04-01 15:09:52 +0200

    change pie

--- backend/create_json.py
+++ backend/create_json.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
 
 import json
+import logging
 
 import cufflinks as cf
 import pandas as pd
 import plotly.express as px
 from pandas.io.pytables import HDFStore
 
+logging.basicConfig(level=logging.INFO)
 cf.go_offline()
 
 
+logging.info("opening data")
 store = HDFStore("store.h5")
 keys = list(store.keys())
 if not keys:
     raise IOError("store is empty")
 
+logging.info("reading data")
 data = []
 for k in keys:
     try:
@@ -29,7 +33,8 @@ store.close()
 data = pd.concat(data)
 data = data.set_index(["timestamp", "owner"])
 
-# stack plot
+
+logging.info("doing stack plot")
 data_to_plot = data.groupby(level=[0, 1])["size"].sum()
 index_sorting = data_to_plot.groupby(level=1).sum().sort_values(ascending=False).index.to_list()
 category_order = {"owner": index_sorting}
@@ -45,36 +50,31 @@ with open("data.json", "w", encoding="utf-8") as f_json_data:
     plt_json = fig.to_json(validate=True, pretty=True)
     f_json_data.write(plt_json)
 
-# pie plot
-latest_data = data.loc[data.index.get_level_values("timestamp").max()]
 
-dataplot = latest_data.reset_index().iplot(
-    kind="pie",
-    labels="owner",
-    values="size",
-    hole=0.4,
-    sort=True,
-    textinfo="percent",
-    asFigure=True,
-)
-dataplot["data"][0]["text"] = ["%.2f Gb" % xx for xx in dataplot["data"][0]["values"]]
-dataplot["data"][0]["hoverinfo"] = "text+label"
-data.iplot(data=dataplot["data"])
+logging.info("doing pie plot")
 
+latest_data = data[
+    data.index.get_level_values("timestamp") == data.index.get_level_values("timestamp").max()
+].reset_index()
+
+latest_data.loc[latest_data['size'] < latest_data['size'].sum() / 100, 'owner'] = 'Others'
+
+
+fig = px.pie(latest_data.reset_index(), values='size', names='owner', hole=.4)
 
 with open("data_pie.json", "w", encoding="utf-8") as f_json_data:
-    plt_json = dataplot.to_json(pretty=True)
+    plt_json = fig.to_json(pretty=True)
     f_json_data.write(plt_json)
 
-
-# scatter plot
-
+"""
+logging.info("doing scatter plot")
 json_merged = {}
 for k, df_v in data.groupby(level=1):
-    plot = df_v.droplevel(1).reindex(data.index.levels[0])['size'].iplot(kind='bar', asFigure=True)
+    plot = df_v.droplevel(1).reindex(data.index.levels[0])["size"].iplot(kind="bar", asFigure=True)
     json_str = plot.to_json(validate=True, pretty=True)
     my_dict = json.loads(json_str)
     json_merged[k] = my_dict
 
-with open('data_scatter.json', 'w', encoding='utf-8') as f:
+with open("data_scatter.json", "w", encoding="utf-8") as f:
     json.dump(json_merged, f, indent=4)
+"""
